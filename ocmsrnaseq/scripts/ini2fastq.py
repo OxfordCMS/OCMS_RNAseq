9deb5f1a91a82f2f9a449e60e4d20cc89a6a89b1
nickilott <nicholas.ilott@kennedy.ox.ac.uk>
2024-02-06 16:37:39 +0000

    reverted change

--- ocmsrnaseq/scripts/ini2fastq.py
+++ ocmsrnaseq/scripts/ini2fastq.py
@@ -19,7 +19,7 @@ import os
 # usage -> python ini2fastq.py config.ini 
 
 # positional arguments and globs for files
-fastqs = glob.glob("*.fastq.gz")
+fastqs = glob.glob("*.fq.gz")
 
 ini_file = sys.argv[1] 
 
